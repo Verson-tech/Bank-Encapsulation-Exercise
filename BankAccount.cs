7f36f8f388633ee0813c654b8a9b3123463d9692
Vira Murray <vira.avramenko@gmail.com>
2022-09-23 15:11:42 -0400

    initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        //create the -->field<-- of type double named balance with a value of 0
+        private double balance = 0;
+        //Define a -->method<-- named Deposit that will accept a double and store that value in the balance field
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+        //Define a -->method<-- named GetBalance that will return the amount stored in the balance field
+        public string GetBalance()
+        {
+            return $"{balance}";
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using BankEncapsulationExercise;
+
+BankAccount account = new BankAccount();
+Console.WriteLine("Enter the amount to be deposited to your account");
+var  amountToDeposit = double.Parse(Console.ReadLine());
+account.Deposit(amountToDeposit);
+Console.WriteLine($"Your balance is {account.GetBalance()}");
