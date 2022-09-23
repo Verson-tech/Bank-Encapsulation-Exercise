9b88842c2baf78e8d7559cc736d95aab0e53fc4c
Vira Murray <vira.avramenko@gmail.com>
2022-09-23 16:12:01 -0400

    fix: amountToWithdraw

--- BankAccount.cs
+++ BankAccount.cs
@@ -11,13 +11,18 @@ namespace BankEncapsulationExercise
         //create the -->field<-- of type double named balance with a value of 0
         private double balance = 0;
         //Define a -->method<-- named Deposit that will accept a double and store that value in the balance field
-        public void Deposit(double amount)
+        public void Deposit(double amountToDeposit)
         {
-            balance = amount;
+            balance += amountToDeposit;
         }
         //Define a -->method<-- named GetBalance that will return the amount stored in the balance field
         public string GetBalance()
         {
+            return $"{balance}";
+        }
+        public string Withdraw(double amountToWithdraw)
+        {
+            balance -= amountToWithdraw;
             return $"{balance}";
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿using BankEncapsulationExercise;
 
 BankAccount account = new BankAccount();
+
 Console.WriteLine("Enter the amount to be deposited to your account");
+
 var  amountToDeposit = double.Parse(Console.ReadLine());
+
 account.Deposit(amountToDeposit);
+
+Console.WriteLine($"Your balance is {account.GetBalance()}");
+Console.WriteLine("Enter the amount to be withdrawn from your account");
+var amountToWithdraw = double.Parse(Console.ReadLine());
+account.Withdraw(amountToWithdraw);
 Console.WriteLine($"Your balance is {account.GetBalance()}");
